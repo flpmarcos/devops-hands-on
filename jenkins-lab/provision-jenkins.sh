b0b3bf16d122f9238f64d7f1cd2babbf3839c0a6
Felipe Marcos <flpmarcos>
2022-09-26 11:37:37 -0300

    57. Introdução ao Jenkins - Instalação do laboratório; 58. Jenkins - Mão na massa - Criação de laboratório;

---
+++ jenkins-lab/provision-jenkins.sh
@@ -0,0 +1,10 @@
+#/bin/bash
+echo "Installing Jenkins and dependencies..."
+yum install epel-release wget -y
+sudo wget --no-check-certificate -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo
+sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
+yum install java-11-openjdk-devel -y
+yum install jenkins -y 
+systemctl daemon-reload
+sudo systemctl start jenkins
+sudo systemctl enable jenkins
