c5b82bc0f09b72d4d6edb74bbdf4aac1adf90a39
Felipe Marcos <flpmarcos>
2022-11-03 12:20:46 -0300

    79. Kubernetes - Configuração do registry privado; 80. Kubernetes - deploy do redis server - parte 1;

--- k3s-lab/provision-k3s.sh
+++ k3s-lab/provision-k3s.sh
@@ -1,2 +1,9 @@
 #/bin/bash
+
+# Cluster
 curl -sfL https://get.k3s.io | sh -s - --cluster-init --tls-san 192.168.1.2 --node-ip 192.168.1.2 --node-external-ip 192.168.1.2
+
+# To get K3S_TOKEN, acess /var/lib/rancher/k3s/server/node-token in cluster 
+
+# Worker
+curl -sfL https://get.k3s.io | K3S_URL=https://192.168.1.2:6443 K3S_TOKEN=mynodetoken sh -
