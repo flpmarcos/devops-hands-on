b7207fb5beedf63ae504ab3a0b4475abc71e6beb
Felipe Marcos <flpmarcos>
2022-11-07 09:07:32 -0300

    85 - Deploy da aplicacao redis - pt 2

--- k3s-lab/provision-k3s.sh
+++ k3s-lab/provision-k3s.sh
@@ -6,4 +6,4 @@ curl -sfL https://get.k3s.io | sh -s - --cluster-init --tls-san 192.168.1.2 --no
 # To get K3S_TOKEN, acess /var/lib/rancher/k3s/server/node-token in cluster 
 
 # Worker
-curl -sfL https://get.k3s.io | K3S_URL=https://192.168.1.2:6443 K3S_TOKEN=K104a053487cc45871a054ed6b83bf12487fc006edae7feb26bf28d6b5b8d3f3792::server:59b13a151bedf5a22913d0ea5cffdea8 sh -s - --node-ip 192.168.1.2 --node-external-ip 192.168.1.2
+curl -sfL https://get.k3s.io | K3S_URL=https://192.168.1.2:6443 K3S_TOKEN=K104a053487cc45871a054ed6b83bf12487fc006edae7feb26bf28d6b5b8d3f3792::server:59b13a151bedf5a22913d0ea5cffdea8 sh -s - --node-ip 192.168.1.3 --node-external-ip 192.168.1.3
